2078efa99abbea331150b03853ad5203c43983c3
Melllzzzz <pacamo.rammel29@gmail.com>
2026-05-27 21:55:35 +0800

    Add EMAIL_TIMEOUT to prevent SMTP from blocking gunicorn worker

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 5
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'pacamo.rammel29@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'ykcc eghf kxmg ekev')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
